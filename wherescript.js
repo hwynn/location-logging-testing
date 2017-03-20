250f174b66934b0780ba3da680fda196e5117888
Harrison Wynn <harrisonwynn42@gmail.com>
2017-03-20 16:28:35 -0500

    webpage now has geolocation functionality.It tells the user's current location but does nothing with it.
    Signed-off-by: Harrison Wynn <harrisonwynn42@gmail.com>

--- wherescript.js
+++ wherescript.js
@@ -1,2 +1,17 @@
-function locateMe(){
+function findMe(){
+	var x = document.getElementById("whereinfo");
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(function(my_position){
+		x.innerHTML = "You're here!<br>" + 
+		"latitude: " + my_position.coords.latitude + 
+		"<br>longitude: " + my_position.coords.longitude +
+		"<br>accuracy: " + my_position.coords.accuracy +
+		"<br>time: " + my_position.timestamp;
+		//x.innerHTML = "You're here!<br>";
+		});
+	}
+	else {
+		x.innerHTML = "Sorry. We couldn't get it to work.";
+	}
+	
 }
