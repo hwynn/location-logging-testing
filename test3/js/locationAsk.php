85c0aa0cb047f1d4e415662cb075c5fa3f056a0e
Harrison Wynn <harrisonwynn42@gmail.com>
2017-05-05 22:40:28 -0500

    added actual data filtering functionality. Conditionals and sql command assembly is handled in the php.
    Signed-off-by: Harrison Wynn <harrisonwynn42@gmail.com>

--- test3/js/locationAsk.php
+++ test3/js/locationAsk.php
@@ -9,8 +9,13 @@
 
 	$mylatitude = floatval($_GET[mylat]);
 	$mylongitude = floatval($_GET[mylong]);
+	$gu = boolval($_GET[gu]);
+	$gm = boolval($_GET[gm]);
+	$gf = boolval($_GET[gf]);
+	$hc = boolval($_GET[hc]);
+	$fl = intval($_GET[fl]);
 	
-	$tellMeThis = "SELECT `Floor`, `Gender`, `Latitude`, `Longitude`,
+	$com1 = "SELECT `Floor`, `Gender`, `Latitude`, `Longitude`,
    ROUND(111.111 * 1000 *
     DEGREES(ACOS(COS(RADIANS('$mylatitude'))
          * COS(RADIANS(`Latitude`))
@@ -18,11 +23,35 @@
          + SIN(RADIANS('$mylatitude'))
          * SIN(RADIANS(`Latitude`))))) AS distance_in_meters, `Handi_access`
 	FROM  `Restrooms` 
-	WHERE  `Floor` <9
+	WHERE  `Floor` <9";
+	
+	$com2 = "";
+	$com2 .= "
+	AND  `Floor` =" . $fl;
+	if(!$gu){
+		$com2 .= "
+		AND NOT `Gender` = 'unisex'";
+	}
+	if(!$gf){
+		$com2 .= "
+		AND NOT `Gender` = 'female'";
+	}
+	if(!$gm){
+		$com2 .= "
+		AND NOT `Gender` = 'male'";
+	}
+	if($hc){
+		$com2 .= "
+		AND `Handi_access` = true";
+	}
+	
+	
+	$com3 = "
 	AND  `Accuracy` <100
 	GROUP   BY ID
 	ORDER BY distance_in_meters";
 	//Did it work?
+	$tellMeThis = "{$com1}{$com2}{$com3}";
 	
 if ($result = mysqli_query($mysqli, $tellMeThis)) {
 
